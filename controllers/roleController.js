b2f1667a92c673c4767bb764372df1dce529728b
Chhan Bunthong <chhan.bunthong@sathapana.com.kh>
2023-10-22 14:22:44 +0700

    update: filter

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -28,8 +28,16 @@ exports.create = async (req, res) => {
 
 exports.list = async (req, res) => {
     try {
-        const roles = await Role.find()
-        response.success(200, { data: roles }, res)
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 5
+        const skip = page - 1
+
+        const name = req.query.name === 'asc' ? 1 : -1
+        const createdAt = req.query.createdAt === 'asc' ? 1 : -1
+
+        const roles = await Role.find({ isDeleted: false }).skip((skip) * limit).limit(limit).sort({ name, createdAt })
+        const totalRole = await Role.count({ isDeleted: false })
+        response.success(200, { data: roles, metaData: { skip, limit, total: totalRole } }, res)
     } catch (error) {
         response.failure(error.code, { message: error.message, fields: error.fields }, res, error)
     }
