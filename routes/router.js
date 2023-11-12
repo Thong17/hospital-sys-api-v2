14e1e86acd833ed08194576b0fca1b36ac64fda4
Chhan Bunthong <chhan.bunthong@sathapana.com.kh>
2023-11-12 12:55:50 +0700

    fix: handle null

--- routes/router.js
+++ routes/router.js
@@ -5,9 +5,9 @@ const security = require('../middlewares/security')
 
 router.get('/upload/:filename', async (req, res) => {
     try {
-        let filename = req.params.filename
-        let bucketName = req.query.bucket
-        let mimetype = req.query.mimetype
+        let filename = req.params.filename ?? 'default.png'
+        let bucketName = req.query.bucket ?? process.env.MINIO_BUCKET
+        let mimetype = req.query.mimetype ?? 'image/jpeg'
         if (['null', 'undefined'].includes(filename)) filename = 'default.png'
         if (['null', 'undefined'].includes(bucketName)) bucketName = process.env.MINIO_BUCKET
         if (['null', 'undefined'].includes(mimetype)) mimetype = 'image/jpeg'
