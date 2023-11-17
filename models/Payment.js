593664ae1c675feeb5c4cbb9c47243faa932c6c9
Chhan Bunthong <chhan.bunthong@sathapana.com.kh>
2023-11-17 09:34:10 +0700

    bugfix

--- models/Payment.js
+++ models/Payment.js
@@ -75,7 +75,8 @@ const schema = mongoose.Schema(
 
 schema.pre('save', async function (next) {
     const currentYear = new Date().getFullYear().toString()
-    const countDocuments = await this.model('Payment').countDocuments({ createdAt: { $lt: new Date(currentYear, 0, 1) } })
+    // const countDocuments = await this.model('Payment').countDocuments({ createdAt: { $lt: new Date(currentYear, 0, 1) } })
+    const countDocuments = await this.model('Payment').countDocuments()
     const invoiceNumber = (countDocuments + 1).toString().padStart(5, '0')
     const generatedInvoice = `${currentYear}-INV${invoiceNumber}`
     const invoice = generatedInvoice?.split(' ').map(key => key?.toLowerCase()).filter(Boolean) || []
